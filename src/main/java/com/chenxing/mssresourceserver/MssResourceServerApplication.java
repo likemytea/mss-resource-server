dae12fe46be364218824a13c04b83d6158614448
huayu <huayu@DESKTOP-N5K2GI1>
2018-09-04 10:29:46 +0800

    aaaa

---
+++ src/main/java/com/chenxing/mssresourceserver/MssResourceServerApplication.java
@@ -0,0 +1,12 @@
+package com.chenxing.mssresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssResourceServerApplication.class, args);
+	}
+}
